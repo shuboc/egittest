e714e1ff86f70eaa74fd3005dff40ea49c4f8c9c
Chris Chao <Chris Chao@Chris-Laptop>
2013-02-18 10:28:31 +0800

    Change!!!

---
+++ Test/src/com/example/test/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Time now = new Time(Time.getCurrentTimezone());
+		now.setToNow();
+		Log.i("monthday", String.valueOf(now.monthDay));
+		Log.i("month", String.valueOf(now.month));
+		Log.i("year", String.valueOf(now.year));
+		Log.i("time", now.format("%k:%M:%S"));
+		
+		
+		Log.i("verbose", "hahahahaha");
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
